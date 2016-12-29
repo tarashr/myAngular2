6546e43fa6cd9dc5d68be58c1728ebb30c7a1d76
Taras Hrynevych <tarashrynevych@gmail.com>
2016-12-29 17:20:43 +0200

    2 chapter

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import ApplicationComponent from './components/application/application-component';
+import CarouselComponent from "./components/carousel/carousel";
+import FooterComponent from "./components/footer/footer";
+import NavbarComponent from "./components/navbar/navbar";
+import ProductItemComponent from "./components/product-item/product-item-component";
+import SearchComponent from "./components/search/search";
+import StarsComponent from "./components/stars/stars-component";
+import {ProductService} from "./services/product-service";
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    declarations: [ ApplicationComponent,
+        CarouselComponent,
+        FooterComponent,
+        NavbarComponent,
+        ProductItemComponent,
+        SearchComponent,
+        StarsComponent],
+    providers: [ProductService],
+    bootstrap: [ ApplicationComponent ]
 })
 export class AppModule { }
