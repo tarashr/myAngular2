acbf55819853bd949d15b2e41bd9c0bdf5a82d28
Taras Hrynevych <tarashrynevych@gmail.com>
2017-01-03 22:41:01 +0200

    classes were refactored

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,7 +5,8 @@ System.config({
         module: "commonjs"},
     map: {
         '@angular': 'node_modules/@angular',
-        'rxjs'    : 'node_modules/rxjs'
+        'rxjs'    : 'node_modules/rxjs',
+        'classes' : 'app/classes'
     },
     paths: {
         'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
@@ -21,6 +22,7 @@ System.config({
         '@angular/compiler'                : {main: 'compiler.umd.min.js'},
         '@angular/router'                  : {main: 'router.umd.min.js'},
         '@angular/platform-browser'        : {main: 'platform-browser.umd.min.js'},
-        '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
+        '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'},
+        'classes'                          : {main: 'index', defaultExtension: 'ts'}
     }
 });
